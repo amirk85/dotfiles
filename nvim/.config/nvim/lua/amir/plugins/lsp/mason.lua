3575debb6c94f3a91dbcd9394ad5bb780eeeded3
Amir Khan <khanamir8589@gmail.com>
2025-03-26 15:02:58 +0530

    updated mason

--- nvim/.config/nvim/lua/amir/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/amir/plugins/lsp/mason.lua
@@ -1,19 +1,38 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    require("mason").setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-        border = "single",
-      },
-    })
-  end,
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		require("mason").setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+				border = "single",
+			},
+			ensure_installed = {
+				-- Language Servers
+				"pyright",                    -- Python
+				"tsserver",                   -- TypeScript/JavaScript
+				"gopls",                      -- Go
+				"sumneko_lua",                -- Lua
+				-- Linters
+				"flake8",                     -- Python Linter
+				"eslint_d",                   -- JavaScript/TypeScript Linter
+				"golangci_lint",              -- Go Linter
+				-- Formatters
+				"black",                      -- Python Formatter
+				"prettier",                   -- Code Formatter
+				"stylua",                     -- Lua Formatter
+				"shfmt",                      -- Shell Script Formatter
+				-- Additional Tools
+				"dockerfile_language_server", -- Dockerfile support
+				"yaml-language-server",       -- YAML support
+			},
+		})
+	end,
 }
